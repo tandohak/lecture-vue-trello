6c51a76b7979edeb806a694e43f62ed8f79fbaa3
yoon pyo <ypyo09@naver.com>
2019-05-01 13:12:23 +0900

    generated router.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
 
-new Vue({
+Vue.use(VueRouter)
+
+new Vue({ 
   el: '#app',
-  render: h => h(App)
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 })
